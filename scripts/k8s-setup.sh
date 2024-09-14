5d4aed8e79133ab6b3cc2a9f10eb7d34ba82f346
kses171 <kses171@gmail.com>
2024-09-14 22:01:34 +0800

    edit k8s sh 3

--- scripts/k8s-setup.sh
+++ scripts/k8s-setup.sh
@@ -19,6 +19,10 @@ microk8s kubectl create namespace your-namespace
 microk8s kubectl config set-context --current --namespace=your-namespace
 microk8s helm install my-open5gs $(pwd)/test1/open5gs-2.2.3/open5gs --namespace your-namespace --values $(pwd)/test1/helms/5gSA-values.yaml
 microk8s helm install my-ueransim-gnb $(pwd)/test1/ueransim-gnb-0.2.6/ueransim-gnb --namespace your-namespace --values $(pwd)/test1/helms/my-gnb-ues-values.yaml
+
+sudo snap install docker
+sudo docker build $(pwd)/test1/php1 -t my-php-app:1.0.0
+sudo docker save my-php-app:1.0.0 > my-php-app.tar
 microk8s ctr image import $(pwd)/test1/php1/my-php-app.tar
 microk8s helm install phpfpm-nginx-release $(pwd)/test1/phpfpm-nginx-chart --namespace your-namespace
 

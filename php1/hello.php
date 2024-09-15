3d9b56c724a005a68718cc6ef4653b967fff7f72
kses171 <kses171@gmail.com>
2024-09-15 14:55:04 +0800

    change stuff in hello.php

--- php1/hello.php
+++ php1/hello.php
@@ -4,7 +4,7 @@
     </head>
     <body>
         <?php echo '<h1>Hello World</h1>'; ?>
+        <?php echo '<h3>Here is the flag!</h3>'; ?>
         <?php echo '<h3>open5gs{4ui4ukd5icummo75rr6r96l1e}</h3>'; ?>
-        <?php phpinfo(); ?>
     </body>
 </html>

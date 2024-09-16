3438be11f82b117a3905e4db08af21a8fdbe3b63
kses171 <kses171@gmail.com>
2024-09-16 09:58:37 +0800

    ctfd change

--- scripts/ctfd-setup.sh
+++ scripts/ctfd-setup.sh
@@ -12,5 +12,5 @@ cd $(pwd)/CTFd
 # Start CTFd using Docker Compose
 sudo docker-compose up
 # install config to CTFd 
-sudo docker cp /home/ubu22/Desktop/ctfd-config-v0.2.zip ctfd-ctfd-1:/tmp/
+sudo docker cp $(pwd)/5G-playground/scripts/ctfd-config-v0.2.zip ctfd-ctfd-1:/tmp/
 sudo docker exec ctfd-ctfd-1 python manage.py import_ctf /tmp/ctfd-config-v0.2.zip

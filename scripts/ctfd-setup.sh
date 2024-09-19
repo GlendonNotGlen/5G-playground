814ac17a57be164ca2c53a47e7e6519638fd0a37
kses171 <kses171@gmail.com>
2024-09-19 12:13:02 +0800

    ctfd description changes

--- scripts/ctfd-setup.sh
+++ scripts/ctfd-setup.sh
@@ -1,3 +1,3 @@
 # install config to CTFd 
 sudo docker cp $(pwd)/5G-playground/scripts/ctfd-config-v0.2.zip ctfd-ctfd-1:/tmp/
-sudo docker exec ctfd-ctfd-1 python manage.py import_ctf /tmp/ctfd-config-v0.2.zip
+sudo docker exec ctfd-ctfd-1 python manage.py import_ctf /tmp/ctfd-config-v0.3.zip

ce6ea82885707f09597334addc348888b0ce500d
ll-xyz-ll <xavieryeo92@gmail.com>
2024-09-23 16:19:50 +0800

    Edited Challenges

--- scripts/ctfd-setup.sh
+++ scripts/ctfd-setup.sh
@@ -1,3 +1,3 @@
 # install config to CTFd 
-sudo docker cp ctfd-config-v0.3.zip ctfd-ctfd-1:/tmp/
-sudo docker exec ctfd-ctfd-1 python manage.py import_ctf /tmp/ctfd-config-v0.3.zip
+sudo docker cp ctfd-config-v0.4.zip ctfd-ctfd-1:/tmp/
+sudo docker exec ctfd-ctfd-1 python manage.py import_ctf /tmp/ctfd-config-v0.4.zip
